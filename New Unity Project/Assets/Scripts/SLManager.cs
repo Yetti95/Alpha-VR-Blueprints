699496db2e1037e68d575f81101ec385fa618ea9
Yetti95 <yetti95@gmail.com>
2017-09-06 23:14:36 -0600

    9/6/2017
    Tested alot of shit

---
+++ New Unity Project/Assets/Scripts/SLManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SLManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SaveLoad.Save();
+        SaveLoad.Load();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ New Unity Project/Assets/Scripts/SaveLoad.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class GameData
+{
+    public static string playerName = "Test";
+    public static int level = 1;
+
+}
+public class SaveLoad
+{
+
+
+    public static void Save()
+    {
+        GameData.playerName = "Aj";
+        GameData.level = 10;
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+        using (FileStream fs = new FileStream("gamesave.bin", FileMode.Create, FileAccess.Write))
+        {
+            binaryFormatter.Serialize(fs, GameData.playerName);
+            binaryFormatter.Serialize(fs, GameData.level);
+
+        }
+    }
+    public static void Load()
+    {
+        if (!File.Exists("gamesave.bin"))
+        {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            using (FileStream fs = new FileStream("gamesave.bin", FileMode.Open, FileAccess.Read))
+            {
+                GameData.playerName = (string)binaryFormatter.Deserialize(fs);
+                GameData.level = (int)binaryFormatter.Deserialize(fs);
+
+            }
+        }
+    }
+}
