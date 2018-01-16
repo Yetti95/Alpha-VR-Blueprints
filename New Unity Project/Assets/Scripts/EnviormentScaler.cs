10dbe9b90ffe317d3e82e166ec84e35c0847a428
Yetti95 <yetti95@gmail.com>
2018-01-15 18:10:05 -0700

    Scaler Init

---
+++ New Unity Project/Assets/Scripts/EnviormentScaler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnviormentScaler : MonoBehaviour {
+    //gets the tracking of controller
+    public SteamVR_TrackedObject trackedObj;
+    private SteamVR_Controller.Device device;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(device.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
+	}
+}
