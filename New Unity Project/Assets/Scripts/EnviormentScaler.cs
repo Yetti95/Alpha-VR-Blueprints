800ba143563e4c9062083b9c8ca44a0b325c1566
Yetti95 <yetti95@gmail.com>
2018-01-15 22:38:34 -0700

    Enviro Scaling Coded

--- New Unity Project/Assets/Scripts/EnviormentScaler.cs
+++ New Unity Project/Assets/Scripts/EnviormentScaler.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class EnviormentScaler : MonoBehaviour {
     //gets the tracking of controller
     public SteamVR_TrackedObject trackedObj;
-    private SteamVR_Controller.Device device;
+    public SteamVR_Controller.Device device;
+    //tells if grips are pressed
+    public bool pressed;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        trackedObj = gameObject.GetComponent<SteamVR_TrackedObject>();
+        device = SteamVR_Controller.Input((int)trackedObj.index);
+        pressed = device.GetPressDown(SteamVR_Controller.ButtonMask.Grip);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		if(device.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
+        
 	}
+    public bool isPressed()
+    {
+       return pressed;
+    }
+
 }
---
+++ New Unity Project/Assets/Scripts/EnviormentScalerManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnviormentScalerManager : MonoBehaviour {
+    public EnviormentScaler[] hands;
+    public GameObject mesh;
+    public bool scalable;
+    //public GameObject obj;
+    // Use this for initialization
+    void Start() {
+
+        Vector3 MeshScale = getMeshScale;
+        Vector3 MeshLoc = getMeshScale;
+        //Thoughts here is as the controllers move away from each other the mesh will scale up and grow. opposite if moved together
+        //thoughts here is as the controllers are move away the mesh will move match the motion of the controller
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        scalable = (hands[0].pressed && hands[1].pressed);
+        if (scalable)
+        {
+            setMeshScale(getDist());
+        }
+        //Iterate over hands, check for inputs
+        foreach (EnviormentScaler h in hands)
+        {
+            if (h.device == null) continue;
+
+            if (h.device.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
+            {
+                print(h.gameObject.name + "pressed down");
+
+            } else if (h.device.GetPressUp(SteamVR_Controller.ButtonMask.Grip))
+            {
+                print(h.gameObject.name + "released");
+            }
+        }
+    }
+
+    //This gets the current scale of the mesh/terrian that we imported
+    public Vector3 getMeshScale
+    {
+        get { return mesh.transform.localScale; }
+    }
+
+    //This gets the current position of the mesh/terrain that we imported
+    public Vector3 getMeshPos
+    {
+        get { return mesh.transform.localPosition; }
+    }
+
+    //Theis sets the current scale of the mesh to a given value
+    public void setMeshScale(Vector3 change)
+    {
+        mesh.transform.localScale = change;
+    }
+
+    //This sets the current position of the mesh to a given value
+    public void setMeshPos(Vector3 change)
+    {
+        mesh.transform.localPosition = change;
+    }
+
+    //gets the distance from each controller
+    public Vector3 getDist()
+    {
+
+        Vector3 left = hands[0].device.transform.pos;
+        Vector3 right = hands[1].device.transform.pos;
+        Vector3 diff = left - right;
+        return diff;
+    }
+}
